db29639d1472f9fd81221e952efffd1dac5c8953
Sall <59910950+ss-o@users.noreply.github.com>
2026-06-01 05:12:37 +0100

    chore(zunit): apply modelines and editorconfig
    * chore(zsh): apply standard modelines across plugins and libraries
    
    * chore(config): add editorconfig formatting baseline
    
    ---------
    
    Co-authored-by: Salvydas Lukosius <ss-o@users.noreply.github.com>

--- .zunit.zsh
+++ .zunit.zsh
@@ -1,3 +1,5 @@
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
 # ZUnit configuration
 ZUNIT_TESTS_DIR='tests'
 ZUNIT_OUTPUT_DIR='tests/_output'
